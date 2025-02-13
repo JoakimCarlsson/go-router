89fbf469e7a22f45f48907745d94e060c7158b71
Joakim Carlsson <joakimcarlsson1994@gmail.com>
2025-02-13 21:51:55 +0100

    remove example

--- example/internal/features/categories/get/response.go
+++
@@ -1,7 +0,0 @@
-package get
-
-import "github.com/joakimcarlsson/go-router/example/internal/features/categories/models"
-
-type Response struct {
-	Category *models.Category `json:"category"`
-}
--- example/internal/features/categories/list/response.go
+++
@@ -1,8 +0,0 @@
-package list
-
-import "github.com/joakimcarlsson/go-router/example/internal/features/categories/models"
-
-type Response struct {
-	Categories []models.Category `json:"categories"`
-	Total      int               `json:"total"`
-}
